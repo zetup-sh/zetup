56a498e6f5301e9399e70f10f3b2af8ae6718eb4
Zane Hitchcox <zwhitchcox@gmail.com>
2019-08-10 20:15:31 -0400

    download from install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "installing zetup..."
@@ -6,26 +6,25 @@ echo "installing zetup..."
 
 default_release="0.0.1-alpha"
 
-arch_info="$(uname -as)"
-case $arch_info in
-  *"x86_64"*) default_arch="amd64"
-  * ) default_arch="i386"
-esac
+if echo "$(uname -as)" | grep -q "x86_64";
+then
+  default_arch="amd64"
+else
+  default_arch="i386"
+fi
 
-os_info="$(uname -as)"
-case $os_info in
-  *"Linux"*) default_os="linux"
-  * ) default_os="darwin"
-esac
+if echo "$(uname -ms)" | grep -q "Linux";
+then
+  default_os="linux"
+else
+  default_os="darwin"
+fi
 
-ZETUP_OS=${ZETUP_OS:default_os}
-ZETUP_ARCH=${ZETUP_ARCH:default_arch}
-ZETUP_RELEASE=${ZETUP_RELEASE:default_release}
+ZETUP_OS=${ZETUP_OS:-$default_os}
+ZETUP_ARCH=${ZETUP_ARCH:-$default_arch}
+ZETUP_RELEASE=${ZETUP_RELEASE:-$default_release}
 
 
 echo "$ZETUP_RELEASE, $ZETUP_ARCH, $ZETUP_OS"
 
-url="https://github.com/zetup-sh/zetup/releases/download/$ZETUP_RELEASE/zetup-$ZETUP_OS-$ZETUP_ARCH"
-
-echo the url is ...
-echo $url
+curl -L -O "https://github.com/zetup-sh/zetup/releases/download/$ZETUP_RELEASE/zetup-$ZETUP_OS-$ZETUP_ARCH"
